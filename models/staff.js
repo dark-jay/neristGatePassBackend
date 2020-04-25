1ad897a7adff3eedef983eb64198b741a89c2c6c
dark-jay <jay14nath@gmail.com>
2020-04-25 15:49:04 +0530

    user, faculty and staff model added

--- models/staff.js
+++ models/staff.js
@@ -16,24 +16,12 @@ const entrySchema = new mongoose.Schema({
 
 const Entry = mongoose.model('Entry', entrySchema);
 
-const Student = mongoose.model('Student', new mongoose.Schema({
+const Staff = mongoose.model('Staff', new mongoose.Schema({
   name: {
     type: String,
     required: true,
     minlength: 4,
     maxlength: 50
-  },
-  rollNo: {
-    type: String,
-    required: true,
-    minlength: 8,
-    maxlength: 12
-  },
-  regNo: {
-    type: String,
-    required: true,
-    minlength: 6,
-    maxlength: 8
   },
   phone: {
     type: Number,
@@ -43,15 +31,13 @@ const Student = mongoose.model('Student', new mongoose.Schema({
   entries: [entrySchema]
 }));
 
-function validateStudent(student) {
+function validateStaff(staff) {
   const schema = Joi.object({
     name: Joi.string().min(5).max(50).required(),
-    rollNo: Joi.string().min(8).max(12).required(),
-    regNo: Joi.string().min(6).max(8).required(),
     phone: Joi.string().min(10).max(12)
   });
 
-  return schema.validate(student);
+  return schema.validate(staff);
 }
 
 function validateEntryPayload(entry) {
@@ -64,7 +50,7 @@ function validateEntryPayload(entry) {
   return schema.validate(entry);
 }
 
-exports.Faculty = Student; 
+exports.Staff = Staff; 
 exports.Entry = Entry;
-exports.validate = validateStudent;
+exports.validate = validateStaff;
 exports.validateEntry = validateEntryPayload;
