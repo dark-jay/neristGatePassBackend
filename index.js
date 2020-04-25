43cdba99f41232461f9fdfa955f3457b6c162897
dark-jay <jay14nath@gmail.com>
2020-04-25 17:52:46 +0530

    added staff and faculty entry in the index

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const config = require('config');
 const mongoose = require('mongoose');
 const students = require('./routes/students');
+const faculties = require('./routes/faculties');
+const staffs = require('./routes/staffs');
 const studentEntry = require('./routes/studentEntry');
+const facultyEntry = require('./routes/facultyEntry');
+const staffEntry = require('./routes/staffEntry');
 const users = require('./routes/users');
 const auth = require('./routes/auth');
 const express = require('express');
@@ -18,7 +22,11 @@ mongoose.connect('mongodb://localhost/gatePassDB')
 
 app.use(express.json());
 app.use('/api/students', students);
+app.use('/api/faculties', faculties);
+app.use('/api/staffs', staffs);
 app.use('/api/studententry', studentEntry);
+app.use('/api/facultyentry', facultyEntry);
+app.use('/api/staffentry', staffEntry);
 app.use('/api/users', users);
 app.use('/api/auth', auth);
  
