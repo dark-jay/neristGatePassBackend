4bbb905be0f45e0ec7d5820981933856e3901ad2
dark-jay <jay14nath@gmail.com>
2020-04-25 15:50:18 +0530

    made necessary changes to index

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+const config = require('config');
 const mongoose = require('mongoose');
 const students = require('./routes/students');
 const studentEntry = require('./routes/studentEntry');
+const users = require('./routes/users');
+const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
+
+if (!config.get('jwtPrivateKey')) {
+  console.error('FATAL ERROR: jwtPrivateKey is not defined.');
+  process.exit(1);
+}
 
 mongoose.connect('mongodb://localhost/gatePassDB')
   .then(() => console.log('Connected to MongoDB...'))
@@ -11,6 +19,8 @@ mongoose.connect('mongodb://localhost/gatePassDB')
 app.use(express.json());
 app.use('/api/students', students);
 app.use('/api/studententry', studentEntry);
+app.use('/api/users', users);
+app.use('/api/auth', auth);
  
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
